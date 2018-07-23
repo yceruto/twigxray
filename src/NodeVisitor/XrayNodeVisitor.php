752c99a20e523d48f6580d176cc35099ddd73420
Yonel Ceruto <yonelceruto@gmail.com>
2018-07-23 11:57:24 -0400

    Support only "*.html.twig" template names

--- src/NodeVisitor/XrayNodeVisitor.php
+++ src/NodeVisitor/XrayNodeVisitor.php
@@ -14,6 +14,11 @@ class XrayNodeVisitor extends \Twig_BaseNodeVisitor
     public const BLOCK = 'BLOCK';
     public const MACRO = 'MACRO';
 
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
     protected function doEnterNode(\Twig_Node $node, \Twig_Environment $env)
     {
         return $node;
@@ -21,27 +26,27 @@ class XrayNodeVisitor extends \Twig_BaseNodeVisitor
 
     protected function doLeaveNode(\Twig_Node $node, \Twig_Environment $env)
     {
-        if ($node instanceof \Twig_Node_Module) {
-            $hash = md5($node->getTemplateName());
+        if ($node instanceof \Twig_Node_Module && $this->supports($templateName = $node->getTemplateName())) {
+            $hash = md5($templateName);
             $node->setNode('display_start', new \Twig_Node(array(
-                new XrayEnterComment(self::TEMPLATE, $hash, null, $node->getTemplateName(), $node->getTemplateLine()),
+                new XrayEnterComment(self::TEMPLATE, $hash, null, $templateName, $node->getTemplateLine()),
                 $node->getNode('display_start'),
             )));
             $node->setNode('display_end', new \Twig_Node(array(
                 new XrayLeaveComment(self::TEMPLATE, $hash),
                 $node->getNode('display_end'),
             )));
-        } elseif ($node instanceof \Twig_Node_Block) {
-            $hash = md5($node->getAttribute('name').$node->getTemplateName());
+        } elseif ($node instanceof \Twig_Node_Block && $this->supports($templateName = $node->getTemplateName())) {
+            $hash = md5($node->getAttribute('name').$templateName);
             $node->setNode('body', new \Twig_Node_Body(array(
-                new XrayEnterComment(self::BLOCK, $hash, $node->getAttribute('name'), $node->getTemplateName(), $node->getTemplateLine()),
+                new XrayEnterComment(self::BLOCK, $hash, $node->getAttribute('name'), $templateName, $node->getTemplateLine()),
                 $node->getNode('body'),
                 new XrayLeaveComment(self::BLOCK, $hash),
             )));
-        } elseif ($node instanceof \Twig_Node_Macro) {
-            $hash = md5($node->getAttribute('name').$node->getTemplateName());
+        } elseif ($node instanceof \Twig_Node_Macro && $this->supports($templateName = $node->getTemplateName())) {
+            $hash = md5($node->getAttribute('name').$templateName);
             $node->setNode('body', new \Twig_Node_Body(array(
-                new XrayEnterComment(self::MACRO, $hash, $node->getAttribute('name'), $node->getTemplateName(), $node->getTemplateLine()),
+                new XrayEnterComment(self::MACRO, $hash, $node->getAttribute('name'), $templateName, $node->getTemplateLine()),
                 $node->getNode('body'),
                 new XrayLeaveComment(self::MACRO, $hash),
             )));
@@ -50,8 +55,8 @@ class XrayNodeVisitor extends \Twig_BaseNodeVisitor
         return $node;
     }
 
-    public function getPriority(): int
+    private function supports(string $name): bool
     {
-        return 0;
+        return '.html.twig' === substr($name, -10);
     }
 }
