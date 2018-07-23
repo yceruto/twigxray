4b969a41bd82057f4bb310dc2fe98d4a589f6a75
Roland Franssen <franssen.roland@gmail.com>
2018-07-23 18:39:48 +0200

    enhance tooltip

--- src/Node/XrayEnterComment.php
+++ src/Node/XrayEnterComment.php
@@ -19,7 +19,7 @@ class XrayEnterComment extends Node implements \Twig_NodeOutputInterface
         $compiler
             ->addDebugInfo($this)
             ->write('echo ')
-            ->string(sprintf("<!--TWIG-START: %s %s %s line:%d-->\n", $this->getAttribute('type'), rtrim($this->getAttribute('hash').' '.$this->getAttribute('name')), $this->getAttribute('template'), $this->lineno))
+            ->string(sprintf("<!--TWIG-START: %s %s %s %d-->\n", $this->getAttribute('type'), rtrim($this->getAttribute('hash').' '.$this->getAttribute('name')), $this->getAttribute('template'), $this->lineno))
             ->raw(";\n")
         ;
     }
